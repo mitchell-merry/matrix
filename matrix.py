ba516581c72b8196e8eab35b64673ceb44fa930c
Mitchell Merry <mitchellmerry145@gmail.com>
2022-03-09 15:39:42 +1100

    Add type hints.

--- matrix.py
+++ matrix.py
@@ -1,12 +1,15 @@
+from __future__ import annotations
+
 class Matrix:
-    def __init__(self, default):
+    def __init__(self, default: list[list[int]]):
+
         # Matrix must be a double array where rows and cols are > 0
         if not isinstance(default, list) or len(default) == 0 or not isinstance(default[0], list) or len(default[0]) == 0:
             raise ValueError("Invalid matrix!")
 
         self.matrix = default
     
-    def append(self, other):
+    def append(self, other: Matrix) -> Matrix:
         """Appends one matrix onto the right of another.
 
         Rows must match in size. Does not modify the original matrix in-place.
@@ -18,21 +21,27 @@ class Matrix:
             raise ValueError("Matrices must have the same number of rows!")
 
         return Matrix([
-            [ x for x in self.matrix[row] ] + [ x for x in other.matrix[row] ] for row, _ in enumerate(self.matrix)
+            self.copyRowByIndex(row) + other.copyRowByIndex(row) for row, _ in enumerate(self.matrix)
         ])
 
-    def interchange(self, rowI, rowJ):
+    def interchange(self, rowI: int, rowJ: int) -> Matrix:
         """Swaps two rows. Returns a new matrix."""
 
         newM = self.copy()
-        newM.matrix[rowI] = [ x for x in self.matrix[rowJ] ]
-        newM.matrix[rowJ] = [ x for x in self.matrix[rowI] ]
-
+        newM.matrix[rowI] = self.copyRowByIndex(rowJ)
+        newM.matrix[rowJ] = self.copyRowByIndex(rowI)
+        
         return newM
 
-    def swap(self, rowI, rowJ): 
+    def swap(self, rowI: int, rowJ: int) -> Matrix: 
         """Swaps two rows. Returns a new matrix. Alias of Matrix#interchange()."""
         return self.interchange(rowI, rowJ)
+
+    def copyRow(self, row: list[int]) -> list[int]:
+        return [ x for x in row ]
+
+    def copyRowByIndex(self, i: int) -> list[int]:
+        return [ x for x in self.matrix[i] ]
 
     def copy(self):
         """Returns a deep copy of the matrix."""
